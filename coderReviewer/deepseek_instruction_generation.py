767c9edd023e86e7fe5fa10b19a0703b7a20e605
Sahil Kore <sahilkore2345@gmail.com>
2025-10-18 02:54:14 +0530

    Changed the temperature

--- coderReviewer/deepseek_instruction_generation.py
+++ coderReviewer/deepseek_instruction_generation.py
@@ -22,16 +22,13 @@ quantization_config = BitsAndBytesConfig(
     load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16, bnb_4bit_quant_type="nf4"
 )
 
-model = (
-    AutoModelForCausalLM.from_pretrained(
-        "deepseek-ai/deepseek-coder-1.3b-instruct",
-        dtype=torch.float16,
-        quantization_config=quantization_config,
-        attn_implementation="sdpa",
-        device_map ="auto" 
-    )
-    .eval()
-)
+model = AutoModelForCausalLM.from_pretrained(
+    "deepseek-ai/deepseek-coder-1.3b-instruct",
+    dtype=torch.float16,
+    quantization_config=quantization_config,
+    attn_implementation="sdpa",
+    device_map="auto",
+).eval()
 
 
 def get_prompt(completion):
@@ -70,43 +67,43 @@ def get_prompt(completion):
     )
 
 
-def generate_instructions(df:pd.DataFrame):
+def generate_instructions(df: pd.DataFrame):
     df["generated_instructions"] = None
-    for task_id ,group_df in tqdm(df.groupby("task_id") , desc = "Generating instructions"):
+    for task_id, group_df in tqdm(
+        df.groupby("task_id"), desc="Generating instructions"
+    ):
         prompts = group_df["instruction_generation_prompt"].to_list()
         inputs = tokenizer(
-            prompts,
-            return_tensors = "pt",
-            padding = True,
-            truncation = True
-        ).to(model.device)                
+            prompts, return_tensors="pt", padding=True, truncation=True
+        ).to(model.device)
         seq_len = inputs["input_ids"].shape[1]
         generated_output = model.generate(
             **inputs,
-            use_cache = True,
-            max_new_tokens = 512,
-            temperature = 0.2,
-            top_p = 0.95,
-            do_sample = True,
-            eos_token_id = tokenizer.eos_token_id,
-            pad_token_id = tokenizer.eos_token_id,
+            use_cache=True,
+            max_new_tokens=512,
+            temperature=0.7,
+            top_p=0.95,
+            do_sample=True,
+            eos_token_id=tokenizer.eos_token_id,
+            pad_token_id=tokenizer.eos_token_id,
         )
-        new_tokens = generated_output[: , seq_len:]
+        new_tokens = generated_output[:, seq_len:]
 
         decoded_instructions = tokenizer.batch_decode(
-            new_tokens,
-            skip_special_tokens = True
+            new_tokens, skip_special_tokens=True
         )
-        df.loc[group_df.index , "generated_instructions"] = decoded_instructions
-                
+        df.loc[group_df.index, "generated_instructions"] = decoded_instructions
+
+
 def run():
     df = pd.read_json("../coder/results/deepseek/eval.jsonl", lines=True)
     df["instruction_generation_prompt"] = df["completion"].apply(get_prompt)
     generate_instructions(df)
-    
-    df.drop(["instruction_generation_prompt"] , axis = 1).to_json(out_path , lines =True , orient = "records")
+
+    df.drop(["instruction_generation_prompt"], axis=1).to_json(
+        out_path, lines=True, orient="records"
+    )
 
 
 if __name__ == "__main__":
-
     run()
